e85a755dd23d410c67b3d7795e88fd9671969c69
Teiji Watanabe <teijiwatanabe01@gmail.com>
2021-03-15 21:30:55 -0400

    New style for index page and count replies - New style for index page - Method to count the comments (replies) of a thread

--- lib/forum/thread/index.ex
+++ lib/forum/thread/index.ex
@@ -1,7 +1,21 @@
 defmodule Forum.Thread.Index do
+  import Ecto.Query
   alias Forum.{Repo, Thread}
 
-  def call() do
-    Repo.all(Thread)
+  def call(option \\ false) do
+    threads = Repo.all(Thread)
+
+    case option do
+      :with_comments_count -> with_comments(threads)
+      _ -> threads
+    end
+  end
+
+  def with_comments(threads), do: Enum.map(threads, fn t -> count_comments(t) end)
+
+  defp count_comments(thread) do
+    query = from comment in "comment", where: comment.thread_id == ^thread.id
+    count = Repo.aggregate(query, :count, :id)
+    thread |> Map.put(:comments_count, count)
   end
 end
